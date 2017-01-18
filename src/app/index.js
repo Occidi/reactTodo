5825a235c66b8f18bea91a61bf1200af447f1dab
Gwendal Jeanson <gwendal.jeanson@yahoo.fr>
2017-01-19 00:20:13 +0100

    Props/State/cyclingData/Nesting

--- src/app/index.js
+++ src/app/index.js
@@ -1,16 +1,69 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 
-//Create component
-var ToDoComponent = React.createClass({
+//Create Memecomponent
+var MemeComponent = React.createClass({
     render: function(){
         return(
             <div>
-                <h1>Test</h1>
-                <p>React</p>
+                <p><strong>Meme:</strong>{this.props.meme.name}</p>
+                <p><strong>Meme quality:</strong>{this.props.meme.quality}</p>
+                <p><strong>Meme popularity:</strong>{this.props.meme.popularity}</p>
             </div>
         );
     }
 });
 
+//Creating ToDoComponent component
+var ToDoComponent = React.createClass({
+    getInitialState:function(){
+        return{
+            todos:['watch','create','fuck'],
+            age:77
+        }
+    },
+    render: function(){
+
+        var younger = setTimeout(function(){
+            this.setState({
+                age:76
+            });
+        }.bind(this),5000);
+
+        var todos = this.state.todos;
+        todos = todos.map(function(item,index){
+            return(
+                <ToDoItem item={item} key={index}/>
+            );
+        });
+
+        return(
+            <div id="todo-list">
+                <p>Epic memes for epic dreams... </p>
+                <p>{this.state.age}</p>
+                <ul>
+                    {todos}
+                </ul>
+            </div>
+        );
+    }//render
+});
+
+//Creating ToDoItem component
+var ToDoItem = React.createClass({
+    render: function()
+    {
+        return(
+            <li>
+                <div className="todo-item">
+                    <span className="item-name">{this.props.item}</span>
+                </div>
+            </li>
+        );
+    }//render
+});
+
+var myMeme = {name: 'InnerMe', quality: 8, popularity:9};
+
+ReactDOM.render(<MemeComponent mssg="memes" meme={myMeme}/>, document.getElementById('meme-wrapper'));
 ReactDOM.render(<ToDoComponent/>, document.getElementById('todo-wrapper'));
