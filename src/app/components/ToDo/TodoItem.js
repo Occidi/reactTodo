eaf3c55b3aa10b93599f2ce5380990ed9eba2514
Gwendal Jeanson <gwendal.jeanson@yahoo.fr>
2017-02-02 01:54:46 +0100

    ajout d'un deuxieme attribut (type)

--- src/app/components/ToDo/TodoItem.js
+++ src/app/components/ToDo/TodoItem.js
@@ -4,14 +4,17 @@ import '../css/ToDoItem.scss';
 // Creating ToDoItem component
 let ToDoItem = React.createClass({
     propTypes: {
-        item: React.PropTypes.string.isRequired,
+        item: React.PropTypes.object.isRequired,
+        name: React.PropTypes.string.isRequired,
+        type: React.PropTypes.string.isRequired,
         onDelete: React.PropTypes.func.isRequired,
     },
     render() {
         return(
             <li>
                 <div className="todo-item">
-                    <span className="item-name">{this.props.item}</span>
+                    <span className="item-name">{this.props.name}</span>
+                    <span className="item-type">{this.props.type}</span>
                     <span className="item-delete"
                     onClick={this.handleDelete}> X </span>
                 </div>
